494fad2d829374b09e37108c8ccee5fb254cb217
RykerZhang <zhangy42@RHIT-R90XZ2SF>
2024-02-11 17:48:58 -0500

    step1

--- csse332-labs-main/xv6-riscv/kernel/proc.c
+++ csse332-labs-main/xv6-riscv/kernel/proc.c
@@ -325,6 +325,64 @@ fork(void)
   return pid;
 }
 
+//
+int pthread_create(int id, void *thread_function, void *arg){
+struct proc *p = myproc();
+struct proc *np;
+int i, pid;
+
+//allocate thread
+if ((np = allocproc()) == 0){
+ return  -1;
+}
+
+
+  // Copy user memory from parent to child.
+  if(uvmcopy(p->pagetable, np->pagetable, p->sz) < 0){
+    freeproc(np);
+    release(&np->lock);
+    return -1;
+  }
+  np->sz = p->sz;
+
+  // copy saved user registers.
+  *(np->trapframe) = *(p->trapframe);
+
+  // Cause fork to return 0 in the child.
+  np->trapframe->a0 = 0;
+
+  // increment reference counts on open file descriptors.
+  for(i = 0; i < NOFILE; i++)
+    if(p->ofile[i])
+      np->ofile[i] = filedup(p->ofile[i]);
+  np->cwd = idup(p->cwd);
+
+  safestrcpy(np->name, p->name, sizeof(p->name));
+
+  pid = np->pid;
+
+
+  np->trapframe->epc = (uint64)thread_function;
+
+
+
+  release(&np->lock);
+
+  acquire(&wait_lock);
+  np->parent = p;
+  release(&wait_lock);
+
+  acquire(&np->lock);
+  np->state = RUNNABLE;
+  release(&np->lock);
+  return pid;
+
+}
+
+
+
+
+
 // Pass p's abandoned children to init.
 // Caller must hold wait_lock.
 void
--- csse332-labs-main/xv6-riscv/kernel/proc.h
+++ csse332-labs-main/xv6-riscv/kernel/proc.h
@@ -104,4 +104,33 @@ struct proc {
   struct file *ofile[NOFILE];  // Open files
   struct inode *cwd;           // Current directory
   char name[16];               // Process name (debugging)
+
+  //extend for thread
+  void *thread_function;
+  void *arg;
+
 };
+
+
+//struct thread{
+ // struct spinlock lock;
+  // p->lock must be held when using these:
+//  enum threadstate state;        // Process state
+ // void *chan;                  // If non-zero, sleeping on chan
+ // int killed;                  // If non-zero, have been killed
+ // int xstate;                  // Exit status to be returned to parent's wait
+ // int tid;                     // Process ID
+ // int waiting;
+  // wait_lock must be held when using this:
+ // struct thread *main;         // Parent process
+
+  // these are private to the process, so p->lock need not be held.
+//  uint64 kstack;               // Virtual address of kernel stack
+ // uint64 sz;                   // Size of process memory (bytes)
+// pagetable_t pagetable;       // User page table
+//  struct trapframe *trapframe; // data page for trampoline.S
+ // struct context context;      // swtch() here to run process
+//  struct file *ofile[NOFILE];  // Open files
+ // struct inode *cwd;           // Current directory
+ // char name[16];               // Process name (debugging)
+//};
